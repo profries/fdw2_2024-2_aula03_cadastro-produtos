b376f1e23d8ea5cd039431044ecdfc55c32258bc
Luis Henrique Ries <prof.ries@gmail.com>
2024-08-29 21:49:02 -0300

    Criacao do projeto, tabela e formulario para cadastro de produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabelaProdutosComponent } from './tabela-produtos/tabela-produtos.component';
+import { FormProdutosComponent } from './form-produtos/form-produtos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabelaProdutosComponent,
+    FormProdutosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
